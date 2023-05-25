5c4ec47c5635dd75912a96c634a902fa158a8f99
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-25 12:44:43 +0300

    Quote tough

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+const *char display(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const *char display(void);
+	return (0);
+}
+
+const *char display(void)
+{
+	*char str = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+
+	return (str);
+}
