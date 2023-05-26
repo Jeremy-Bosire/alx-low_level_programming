916ac05ce17d1f795ccc1672098816b878eccb18
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-26 23:05:00 +0300

    Last changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,19 +9,13 @@ int main(void)
 {
 	char c;
 
-	c = 'a';
-	while (c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c == 'e')
-		{
-			continue;
-		}
-		if (c == 'q')
+		if (c == 'e' || c == 'q')
 		{
 			continue;
 		}
 		putchar(c);
-		c++;
 	}
 	putchar('\n');
 	return (0);
