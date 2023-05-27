e36eae676719b7d8124f4fd03d7c94d113065633
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-27 03:10:24 +0300

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#define MAXSTRING 80
+#include <unistd.h>
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char function[] = "_putchar";
+
+	for (int i = 0; i < MAXSTRING; i++)
+	{
+		_putchar(function[i]);
+		if (function[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+	}
+	return (0);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks about Functions and nested loops in C.
