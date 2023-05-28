3b7ba4c2487b2779d992d000d8e519727a760a6e
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-28 20:41:05 +0300

    alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
  * _islower - Checks for lowercase characters
  *
  * Return: Either 0 or 1 (Based on input)
+ *
+ * c - character value
  */
 int _islower(int c)
 {
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic characters
+ *
+ * Return: Either 0 or 1 (Depends on the input)
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
