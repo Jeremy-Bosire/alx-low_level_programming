2ed697700c77646e81d7078ee864369f7af8d4e0
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-29 20:06:06 +0300

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ *
+ * @s: String variable
+ */
+void print_rev(char *s)
+{
+	for (;*s + 1 = '\0'; s--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
