da803f307ed3806921e92d8f4733bd270505716f
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-29 21:41:18 +0300

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: String variable
+ */
+void rev_string(char *s)
+{
+	int n, start, end;
+
+	n = 0;
+	while (*s != '\0')
+	{
+		n++;
+	}
+	start = 0;
+	end = n - 1;
+	while (start < end)
+	{
+		int t = s[start];
+
+		s[start] = s[end];
+		s[end] = s[start];
+		start++;
+		end--;
+	}
+}
