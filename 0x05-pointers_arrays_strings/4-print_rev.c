32845eeadcd6f7be35c67be081703877d65c564b
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-29 20:31:04 +0300

    aki

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int n;
+	int i, n;
 
 	while (s[n] != '\0')
 	{
 		n++;
 	}
-	for (int i = n - 1; i >= 0; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
