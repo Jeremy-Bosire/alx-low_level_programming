180d38f436bf0978fd291b78db27c58ed1db75cb
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-29 19:37:18 +0300

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * Return: Length of the inputted string
+ * @s: String variable
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = sizeof(s);
+	return (n);
+}
