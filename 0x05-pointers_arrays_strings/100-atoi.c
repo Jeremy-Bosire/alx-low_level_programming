16c93d17927fbd2bbd23d824ecc278213f5baefe
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-05-30 13:41:14 +0300

    _atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - Function that converts a string to an integer
+ * @s: String variable
+ * Return: Either integer array or 0 depending on a condition
+ */
+int _atoi(char *s)
+{
+	signed int z;
+	int n, k, i, j;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (k = 0; k < n; k++)
+	{
+		z[k] = s[k];
+	}
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j < n; j++)
+		{
+			if (s[j] == i)
+			{
+				return (*z);
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	return (0);
