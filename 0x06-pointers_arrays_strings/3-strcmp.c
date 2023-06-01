69e6b068e9a63a0307b32d76630546a024d35024
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-02 02:05:11 +0300

    Logical vision

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,33 +9,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_length, s2_length, dif, i, j, m, n;
+	int i;
 
-	s1_length = 0;
-	s2_length = 0;
-	dif = 0;
 	i = 0;
-	j = 0;
-	m = 0;
-	n = 0;
-	while (s1[s1_length] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		s1_length++;
-	}
-	while (s2[s2_length] != '\0')
-	{
-		s2_length++;
-	}
-	while (i < s1_length)
-	{
-		m = m + s1[i];
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	while (j < s2_length)
-	{
-		n = n + s2[j];
-		j++;
-	}
-	dif = m - n;
-	return (dif);
+	return (s1[i] - s2[i]);
 }
