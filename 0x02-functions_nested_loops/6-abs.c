3ac24721853b3c528c203d130fb3f4312b78fc21
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 04:32:52 +0300

    Testing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * Return: Absolute value of integer
+ * @x: Integer variable
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = unsigned int x;
+	}
+	return (x);
+}
