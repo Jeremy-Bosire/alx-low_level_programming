a14521ad4bfd6906a1d866f04c1ceaea17dbe8ef
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 18:58:17 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * Return: Resulting string
+ * @dest: String variable
+ * @src: String variable
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, m, n, s;
+
+	n = 0;
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	m = 0;
+	while (src[m] != '\0')
+	{
+		m++;
+	}
+	s = m + n;
+	i = m + 1;
+	j = 0;
+	while (i > m)
+	{
+		if (i == s)
+		{
+			dest[i] = '\0';
+			break;
+		}
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks about More pointers, arrays and strings.
