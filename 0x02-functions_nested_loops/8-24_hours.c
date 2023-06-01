85f3a1633d66262ca78d268597f8c88e698a7bce
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 13:57:34 +0300

    Automatic clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i, j, m, n, p, q;
+
+	for (i = 0; i < 24; i++)
+	{
+		if (i > 9)
+		{
+			q = i / 10;
+			p = i % 10;
+			_putchar(q + '0');
+			_putchar(p + '0');
+		}
+		else
+		{
+			_putchar(0 + '0');
+			_putchar(i + '0');
+		}
+		_putchar(':');
+		for (j = 0; j < 60; j++)
+		{
+			if (j > 9)
+			{
+				n = j / 10;
+				m = j % 10;
+				_putchar(n + '0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar(0 + '0');
+				_putchar(j + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
