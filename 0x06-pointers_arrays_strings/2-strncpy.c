eea3674a62207d53d8703ea2b28439dc8a3b7497
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 23:48:05 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ *
+ * Return: Resultant string after copying
+ * @dest: String variable
+ * @src: String variable
+ * @n: Integer variable
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int src_length, i;
+
+	src_length = 0;
+	while (src[src_length] != '\0')
+	{
+		src_length++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
