67300c89be84e9ca6a2aed6e2f3524d727a3adbb
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 04:55:04 +0300

    Round 1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @x: Integer variable
+ * Return: The last digit of a number
+ */
+int print_last_digit(int x)
+{
+	int n;
+
+	if (!(x > -10 && x < 10))
+	{
+		n = x % 10;
+		return (n);
+	}
+	else if (x >= 0 && x < 10)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
