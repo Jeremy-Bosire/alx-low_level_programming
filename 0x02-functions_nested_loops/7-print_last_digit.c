b94d164f6033a78c47ef5804646339fc7322075a
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-01 13:33:26 +0300

    Massive Change

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,33 +8,16 @@
  */
 int print_last_digit(int x)
 {
-	int n, p;
+	int last;
 
-	if (!(x > -10 && x < 10))
+	if (x < 0)
 	{
-		if (x <= -10)
-		{
-			p = -x;
-			n = p % 10;
-			_putchar (n + '0');
-			return (n);
-		}
-		else
-		{
-			n = x % 10;
-			_putchar (n + '0');
-			return (n);
-		}
-	}
-	else if (x >= 0 && x < 10)
-	{
-		_putchar (x + '0');
-		return (x);
+		last = -x % 10;
 	}
 	else
 	{
-		p = -x;
-		_putchar (p + '0');
-		return (p);
+		last = x % 10;
 	}
+	_putchar (last + '0');
+	return (last);
 }
