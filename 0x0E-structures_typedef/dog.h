62a98f7aaf09b411b02bb9367d562163f9e783d4
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-20 11:54:12 +0300

    Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Contains the information bundle required for dogs
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
