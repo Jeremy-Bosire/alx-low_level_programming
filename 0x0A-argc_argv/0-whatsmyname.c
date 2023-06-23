9446c9606a3d7c38b5a1483babaa1b6e238378c1
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-06-23 17:49:21 +0300

    Task number 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguements
+ * @argv: Array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
